5d0aa816483206870f136c6a87c637e7a30fba73
GmashaN <KillerPoteto@yandex.ru>
2025-12-17 10:57:59 +0300

    fix gc for Windows

--- UMKA-VM/garbage_collector/garbage_collector.h
+++ UMKA-VM/garbage_collector/garbage_collector.h
@@ -10,7 +10,9 @@
 #include <cstdint>
 
 #if defined(_WIN32)
-  #define NOMINMAX
+  #ifndef NOMINMAX
+    #define NOMINMAX
+  #endif
   #include <windows.h>
 #elif defined(__unix__) || defined(__APPLE__)
   #include <unistd.h>
